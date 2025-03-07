37bb2c9ab5720525818e842d22b0d8e6ae1f36fe
Trun97 <tarun7283@gmail.com>
2025-03-07 17:15:06 +0100

    links in navbar

--- src/pages/Nav/Nav.jsx
+++ src/pages/Nav/Nav.jsx
@@ -1,10 +1,15 @@
 import './Nav.css';
+import {Link} from "react-router-dom";
 
 function Nav() {
     return (
-        <h1>Navigatie bar</h1>
-
-
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/overzichtspagina">Alle posts</Link></li>
+                <li><Link to="/newpost">Nieuwe Post maken</Link></li>
+            </ul>
+        </nav>
     )
 }
 
