5ca4f47eed30733179765bdd4fb4565e20261ffe
Trun97 <tarun7283@gmail.com>
2025-03-10 14:37:24 +0100

    opdr 3 af

--- src/pages/NewPost/NewPost.jsx
+++ src/pages/NewPost/NewPost.jsx
@@ -1,8 +1,62 @@
 import './NewPost.css';
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function NewPost() {
+    const [formTitle, setFormTitle] = useState("");
+    const [formSubtitle, setFormSubtitle] = useState("");
+    const [formNames, setFormNames] = useState("");
+    const [formBlogpost, setFormBlogpost] = useState("");
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+
+    function handleSubmit (e) {
+        e.preventDefault();
+
+        if (!formTitle.trim() || !formSubtitle.trim() || !formNames.trim()) {
+            setError("Alle velden moeten worden ingevuld");
+        }
+        if (formBlogpost.length < 300 || formBlogpost > 2000) {
+            setError("De blogpost moet minimaal 300 en maximaal 2000 karaktes bevatten")
+        }
+
+
+        const postInformation = {
+            title: formTitle,
+            subtitle: formSubtitle,
+            content: formBlogpost,
+            author: formNames,
+            comments: 0,
+            shares: 0
+        }
+        navigate("/overzichtspagina");
+        console.log(postInformation);
+    }
+
+
+
     return (
-        <div>Alle nieuwe posts</div>
+        <>
+            <h1>Post toevoegen</h1>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title
+                    <input type="text" id="title" name="title" value={formTitle} onChange={(e) => setFormTitle(e.target.value)}/>
+                </label>
+                <label htmlFor="subtitle">Subtitle
+                    <input type="text" id="subtitle" name="subtitle" value={formSubtitle} onChange={(e) => setFormSubtitle(e.target.value)}/>
+                </label>
+                <label htmlFor="names">Naam en achternaam
+                    <input type="text" id="names" name="names" value={formNames} onChange={(e) => setFormNames(e.target.value)}/>
+                </label>
+                <label htmlFor="blogpost">Blogpost
+                    <textarea id="blogpost" name="blogpost" value={formBlogpost} onChange={(e) => setFormBlogpost(e.target.value)}/>
+                </label>
+                <button type="submit">Toevoegen</button>
+            </form>
+            </>
     )
 }
 
