38e04ea183001c8a6fcb7d3f3c49e9b9083b089d
Trun97 <tarun7283@gmail.com>
2025-03-15 14:05:14 +0100

    opdr 2

--- src/pages/Overzichtspagina/Overzichtspagina.jsx
+++ src/pages/Overzichtspagina/Overzichtspagina.jsx
@@ -1,19 +1,42 @@
 import './Overzichtspagina.css';
 import { Link } from "react-router-dom";
-import posts from "../../constants/data.json";
+// import posts from "../../constants/data.json";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
 
 function Overzichtspagina() {
+    const [blogs, setBlogs] = useState([]);
+    const [error, setError] = useState("")
+
+    async function fetchData() {
+        try {
+            const response = await axios.get("http://localhost:3000/posts");
+            console.log(response.data);
+            setBlogs(response.data);
+        } catch (err) {
+            setError(`Er is iets misgegaan: ${err.message}. Probeer het later opnieuw`);
+            console.error(`Er is iets misgegaan: ${err.message}`);
+
+        }
+    }
+
+    useEffect(() => {
+        void fetchData()
+    },[]);
+
+
     return (
         <div>
             <h1>Overzichtspagina</h1>
-            <h2>Bekijk alle {posts.length} posts op het platform</h2>
+            <h2>Bekijk alle {blogs.length} posts op het platform</h2>
             <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Link to={`/posts/${post.id}`}>
-                            <p>{post.title} ({post.author})</p>
+                {error ? (<p>{error}</p>) : blogs.map((blog) => (
+                    <li key={blog.id}>
+                        <Link to={`/posts/${blog.id}`}>
+                            <p>{blog.title} ({blog.author})</p>
                         </Link>
-                        <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
+                        <p>{blog.comments} reacties - {blog.shares} keer gedeeld</p>
                     </li>
                 ))}
             </ul>
